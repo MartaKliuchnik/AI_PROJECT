a76313171919dd14542d59c8d56109eeffe6e3de
Marta <martasagun95@gmail.com>
2023-07-30 15:13:46 +0200

    title for page was changed

--- src/components/WelcomText/index.jsx
+++ src/components/WelcomText/index.jsx
@@ -8,12 +8,12 @@ export default function WelcomText() {
     <section className={[s.welcome_section, 'wrapper'].join(' ')}>
         <div className={s.block_why}>
           <h1>Analyze customer reviews</h1>
-          <p>"AI-powered insights at your fingertips Understanding Your Customers through Review Analysis
-            As a business, it's crucial to know what your customers think about your products or services. This information can help you identify areas for improvement, as well as what you're doing right. One of the best ways to gather this information is by analyzing customer reviews."
+          <p>AI-powered insights at your fingertips Understanding Your Customers through Review Analysis
+            As a business, it's crucial to know what your customers think about your products or services. This information can help you identify areas for improvement, as well as what you're doing right. One of the best ways to gather this information is by analyzing customer reviews.
         </p>
-        <Link to='/about_us'>
+        {/* <Link to='/about_us'>
           <Button>Read more</Button>
-        </Link>
+        </Link> */}
       </div>   
     </section>
   )
